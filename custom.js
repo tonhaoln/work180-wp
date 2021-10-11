13051a395238d434f079225ab9ffa39dc33f793d
Antonio <antonioalmeida084@gmail.com>
2021-10-11 17:47:43 +1100

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,21 +1,19 @@
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet">
-<script>
-    jQuery(function($) {
+jQuery(function($) {
+    
+    //Products and Services Table READ MORE
+    $('.table-control .row-header').click(function() {
+        $(this).toggleClass('active');
+        $(this).next('.row-content').toggleClass('show');
+    });
 
-        $('.table-control .row-header').click(function() {
-            $(this).toggleClass('active');
-            $(this).next('.row-content').toggleClass('show');
-        });
+    //Anchor general READ MORE
+    $('.reveal-more-control .et_pb_module_header').click(function() {
+        $(this).toggleClass('active');
+        $(this).next('.et_pb_blurb_description').toggleClass('show');
+    });
 
-        $('.reveal-more-control .et_pb_module_header').click(function() {
-            $(this).toggleClass('active');
-            $(this).next('.et_pb_blurb_description').toggleClass('show');
-        });
-
-        //$('.breadcrumbs-control span[typeof="BreadcrumbList"]').click(function() {
-        //$(this).toggleClass('active');			
-        //});
+    //Breadcrumbs TOGGLE MOBILE
+    if (window.matchMedia('(max-width: 768px)').matches) {
 
         var s = $('.breadcrumbs-control span[typeof="BreadcrumbList"]').html();
 
@@ -26,16 +24,14 @@
         $("a.br-expand").click(function() {
             $('.breadcrumbs-control span[typeof="BreadcrumbList"]').html(s);
         });
+    }
 
-    });
-</script>
+    //$('.breadcrumbs-control span[typeof="BreadcrumbList"]').click(function() {
+    //$(this).toggleClass('active');			
+    //});
 
-<script>
-    jQuery(function($) {
+    //$('.fa').click(function() {
+        //alert('The button was clicked!'); //$(this).toggleClass('active');			
+    //});
 
-        $('.fa').click(function() {
-            alert('The button was clicked!'); //$(this).toggleClass('active');			
-        });
-
-    });
-</script>
+});
