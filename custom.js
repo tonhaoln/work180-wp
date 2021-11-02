177b8b053c683242edfac8f0ed403ec6b0091bd2
Antonio <antonioalmeida084@gmail.com>
2021-11-02 18:00:34 +1100

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,3 +1,6 @@
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet">
+<script>	
 jQuery(function($) {
     
     //Products and Services Table READ MORE
@@ -35,3 +38,4 @@ jQuery(function($) {
     //});
 
 });
+</script>
