e78f6b034edb33098c7c72abf15850db43910b2c
Antonio <antonioalmeida084@gmail.com>
2021-11-04 15:25:42 +1100

    Update functions.php

--- functions.php
+++ functions.php
@@ -10,3 +10,22 @@ function yourprefix_menu_arrow($item_output, $item, $depth, $args) {
     return $item_output;
 }
 add_filter('walker_nav_menu_start_el', 'yourprefix_menu_arrow', 10, 4);
+
+/**
+ * Register new image sizes
+ */
+function new_image_sizes() {
+  add_image_size( 'blog-landscape', 580, 300, true );  
+  add_image_size( 'blog-square', 300, 300, true );  
+}
+
+add_action('after_setup_theme', 'new_image_sizes'); 
+
+add_filter( 'image_size_names_choose', 'my_custom_sizes' );
+
+function my_custom_sizes( $sizes ) {
+    return array_merge( $sizes, array(
+        'blog-landscape' => __( 'Blog Landscape' ),
+		'blog-square' => __( 'Blog Square' ),
+    ));
+}
