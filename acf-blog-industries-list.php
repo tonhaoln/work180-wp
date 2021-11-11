0b8883ca04e32c620dad230472c641040a7c7ec6
Antonio <antonioalmeida084@gmail.com>
2021-11-11 14:29:28 +1100

    Create acf-blog-industries-list.php

---
+++ acf-blog-industries-list.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * ACF select field industry list for blog posts 
+ *
+?>
+
+/for-women/job-search/
+/for-women/job-search/automotive
+/for-women/job-search/chemicals
+/for-women/job-search/construction
+/for-women/job-search/engineering
+/for-women/job-search/telecommunications
+/for-women/job-search/advertising-media-and-publishing
+/for-women/job-search/agriculture-forestry-and-fisheries
+/for-women/job-search/architecture-and-design
+/for-women/job-search/art-music-and-entertainment
+/for-women/job-search/aviation-and-aerospace
+/for-women/job-search/banking-investment-and-finance
+/for-women/job-search/consulting-and-professional-services
+/for-women/job-search/defence-and-emergency
+/for-women/job-search/education-and-training
+/for-women/job-search/fmcg-and-consumer-goods
+/for-women/job-search/healthcare-and-medical
+/for-women/job-search/hospitality-and-tourism
+/for-women/job-search/hr-and-recruitment
+/for-women/job-search/import-and-export
+/for-women/job-search/information-services-and-records-management
+/for-women/job-search/insurance-and-superannuation
+/for-women/job-search/it-digital-and-online-media-services
+/for-women/job-search/legal-services
+/for-women/job-search/local-state-and-federal-government
+/for-women/job-search/manufacturing-and-operations
+/for-women/job-search/mining-resources-and-energy
+/for-women/job-search/real-estate-and-property
+/for-women/job-search/retail-and-fashion
+/for-women/job-search/science-and-biotechnology
+/for-women/job-search/sports-wellness-and-fitness
+/for-women/job-search/sustainability-and-environment
+/for-women/job-search/trades-and-services
+/for-women/job-search/transport-shipping-and-logistics
+
+
+
+
+Select
+automotive : Search for the latest jobs in the <b>Automotive</b> industry.
+chemicals : Search for the latest jobs in the <b>Chemicals</b> industry.
+construction : Search for the latest jobs in the <b>Construction</b> industry.
+engineering : Search for the latest jobs in the <b>Engineering</b> industry.
+telecommunications : Search for the latest jobs in the <b>Telecommunications</b> industry.
+advertising-media-and-publishing : Search for the latest jobs in the <b>Advertising, media & publishing</b> industry.
+agriculture-forestry-and-fisheries : Search for the latest jobs in the <b>Agriculture, forestry & fisheries</b> industry.
+architecture-and-design : Search for the latest jobs in the <b>Architecture & design</b> industry.
+art-music-and-entertainment : Search for the latest jobs in the <b>Art, music & entertainment</b> industry.
+aviation-and-aerospace : Search for the latest jobs in the <b>Aviation & aerospace</b> industry.
+banking-investment-and-finance : Search for the latest jobs in the <b>Banking, investment & finance</b> industry.
+consulting-and-professional-services : Search for the latest jobs in the <b>Consulting & professional services</b> industry.
+defence-and-emergency : Search for the latest jobs in the <b>Defence & emergency</b> industry.
+education-and-training : Search for the latest jobs in the <b>Education & training</b> industry.
+fmcg-and-consumer-goods : Search for the latest jobs in the <b>Fmcg & consumer goods</b> industry.
+healthcare-and-medical : Search for the latest jobs in the <b>Healthcare & medical</b> industry.
+hospitality-and-tourism : Search for the latest jobs in the <b>Hospitality & tourism</b> industry.
+hr-and-recruitment : Search for the latest jobs in the <b>Hr & recruitment</b> industry.
+import-and-export : Search for the latest jobs in the <b>Import & export</b> industry.
+information-services-and-records-management : Search for the latest jobs in the <b>Information services & records management</b> industry.
+insurance-and-superannuation : Search for the latest jobs in the <b>Insurance & superannuation</b> industry.
+it-digital-and-online-media-services : Search for the latest jobs in the <b>It, digital & online media services</b> industry.
+legal-services : Search for the latest jobs in the <b>Legal services</b> industry.
+local-state-and-federal-government : Search for the latest jobs in the <b>Local, state & federal government</b> industry.
+manufacturing-and-operations : Search for the latest jobs in the <b>Manufacturing & operations</b> industry.
+mining-resources-and-energy : Search for the latest jobs in the <b>Mining, resources & energy</b> industry.
+real-estate-and-property : Search for the latest jobs in the <b>Real estate & property</b> industry.
+retail-and-fashion : Search for the latest jobs in the <b>Retail & fashion</b> industry.
+science-and-biotechnology : Search for the latest jobs in the <b>Science & biotechnology</b> industry.
+sports-wellness-and-fitness : Search for the latest jobs in the <b>Sports, wellness & fitness</b> industry.
+sustainability-and-environment : Search for the latest jobs in the <b>Sustainability & environment</b> industry.
+trades-and-services : Search for the latest jobs in the <b>Trades & services</b> industry.
+transport-shipping-and-logistics : Search for the latest jobs in the <b>Transport, shipping & logistics</b> industry.
