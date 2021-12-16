46362e7de92d0567315e7192da501f1e3325152c
Antonio <antonioalmeida084@gmail.com>
2021-12-16 13:32:30 +1100

    Update _functions.php

--- _functions.php
+++ _functions.php
@@ -37,10 +37,8 @@ function my_custom_sizes( $sizes ) {
 /**
  * Search Function to only search posts
  */
-function SearchFilter($query) {
-    if ($query->is_search) {
-        $query->set('post_type', 'post');
-    }
-    return $query;
+function remove_pages_from_search() {
+    global $wp_post_types;
+    $wp_post_types['page']->exclude_from_search = true;
 }
-add_filter('pre_get_posts','SearchFilter');
+add_action('init', 'remove_pages_from_search');
